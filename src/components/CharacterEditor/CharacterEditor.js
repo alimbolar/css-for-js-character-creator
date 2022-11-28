bb62f8ad01d180922be2ade3463c4baf4a2ae231
Alim Bolar <alimbolar@gmail.com>
2022-11-28 11:22:17 +0800

    updated after checking solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,7 +25,9 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveBar}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        {/* TODO perspectiveBar */}
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
@@ -33,8 +35,6 @@ function App() {
             What sort of adventure will you embark on?{" "}
           </p>
         </header>
-        {/* TODO perspectiveBar */}
-        <div className={styles.perspectiveBar}></div>
         <div className={styles.controlColumn}>
           <ControlPane
             title="Bodies"
